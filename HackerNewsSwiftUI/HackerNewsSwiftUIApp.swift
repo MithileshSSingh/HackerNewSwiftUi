a912f78466d7bef5d4449ebfd83aa20ce8a0be81
mithileshssingh <mithilesh.singh@blackbuck.com>
2024-03-10 09:55:31 +0530

    Initial Commit

---
+++ HackerNewsSwiftUI/HackerNewsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsSwiftUIApp.swift
+//  HackerNewsSwiftUI
+//
+//  Created by Mithilesh Singh on 10/03/24.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
